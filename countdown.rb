5294dd052d9b488478eb50c9718551b5b2f503cc
Adam Dudley <dudleyadam@gmail.com>
2018-11-21 21:39:42 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 def countdown(some_integer)
   counter = some_integer
   while counter >= 0
-    puts "#{some_integer} SECOND(S)!"
+    puts "#{counter} SECOND(S)!"
     counter -= 1
   end
 "HAPPY NEW YEAR!"
@@ -10,7 +10,7 @@ end
 def countdown_with_sleep(some_other_integer)
   counter = some_other_integer
   while counter > 0
-    puts "#{some_other_integer} SECOND(S)!"
+    puts "#{counter} SECOND(S)!"
     sleep 1
     counter -= 1
   end
